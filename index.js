2f66219cf2830b000502d6c2d80581656603a84b
Tejas1kawale <55426899+Tejas1kawale@users.noreply.github.com>
2023-02-10 01:45:44 +0530

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express =require ("express");
-const app=express();
+const http=require('http');
+const  app=requie('./app');
 const port=process.env.PORT || 3000
+const server=http.createServer(app);
 app.get("/",(req,res)=>{
     res.send("Hello I am live");
 });
+
 app.listen(port,()=>{
 console.log("Hello I am live again")
 })
